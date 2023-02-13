da7d2e39302c95ba83fdb954a4dfd20be1c2dac8
mick605 <mick605.0@gmail.com>
2023-02-13 19:35:53 +0100

    Improved CSS, add drag&drop mobile

--- components/app-main/app-main.js
+++ components/app-main/app-main.js
@@ -10,14 +10,24 @@ export default class AppMain extends BaseComponent {
     constructor() {
         super();
 
+        this.counters = [...this.shadowRoot.querySelectorAll(".counter")];
         this.reserve = this.shadowRoot.getElementById("reserve");
 
-        this.shadowRoot.addEventListener('dragstart', (e) => this.dragstart_handler(e));
-        this.shadowRoot.addEventListener('dragover', (e) => this.dragover_handler(e));
-        this.shadowRoot.addEventListener('drop', (e) => this.drop_handler(e));
+        this.shadowRoot.addEventListener('dragstart', (e) => this.dragstartHandler(e));
+        this.shadowRoot.addEventListener('dragenter', (e) => this.dragenterHandler(e));
+        this.shadowRoot.addEventListener('dragover', (e) => this.dragoverHandler(e));
+        this.shadowRoot.addEventListener('drop', (e) => this.dropHandler(e));
+        
+        try {
+            this.placement = JSON.parse(localStorage.getItem('placement'));
+            this.refreshDom();
+        } catch {
+            this.placement = new Array(19).fill(null);
+            this.refreshDom();
+        }
     }
 
-    dragstart_handler(ev) {
+    dragstartHandler(ev) {
         const evPath = ev.composedPath();
         const token = evPath.find(el => el.classList?.contains("token"));
         const slot = evPath.find(el => el.classList?.contains("slot"));
@@ -30,46 +40,67 @@ export default class AppMain extends BaseComponent {
         ev.dataTransfer.effectAllowed = "move";
     }
 
-    dragover_handler(ev) {
+    dragenterHandler(ev) {
+        ev.preventDefault()
+    }
+
+    dragoverHandler(ev) {
         ev.preventDefault();
         const slot = ev.composedPath().find(el => el.classList?.contains("slot") || el.id === "reserve"); 
         ev.dataTransfer.dropEffect = slot ? "move" : "none";
     }
 
-    drop_handler(ev) {
+    dropHandler(ev) {
         const evPath = ev.composedPath();
         const slot = evPath.find(el => el.classList?.contains("slot"));
 
         const dragdata = JSON.parse(ev.dataTransfer.getData("application/hexagon-puzzle"));
-        const token = this.shadowRoot.querySelector(`.token[data-value="${dragdata.token}"`);
 
         if (!slot) {
             if (!evPath.includes(this.reserve)) return;
-            this.putInReserve(token);
+            this.placement[dragdata.token - 1] = null;
         } else {
-            const existingToken = slot.querySelector('.token');
+            const existingToken = this.placement.indexOf(slot.dataset.position) + 1;
             if (existingToken) {
-                const fromSlot = this.shadowRoot.querySelector(`.slot[data-position="${dragdata.fromSlot}"`);
-                if (fromSlot) {
-                    fromSlot.appendChild(existingToken);
-                } else {
-                    this.putInReserve(existingToken);
-                }
+                this.placement[existingToken - 1] = dragdata.fromSlot;
             }
 
-            slot.appendChild(token);
+            this.placement[dragdata.token - 1] = slot.dataset.position;
+        }
+
+        this.refreshDom();
+        localStorage.setItem("placement", JSON.stringify(this.placement));
+    }
+
+    calculateSum(arr) {
+        return arr.reduce((total, current) => {
+            return total + current;
+        }, 0);
+    }
+
+    toSignedString(num) {
+        return num > 0 ? "+" + num : "" + num;
+    }
+
+    updateCalculations() {
+        for (const counterDom of this.counters) {
+            const positions = [...counterDom.dataset.count];
+            const delta = 38 - this.calculateSum(positions.map(slotName => this.placement.indexOf(slotName) + 1));
+            counterDom.dataset.result = this.toSignedString(delta);
+            counterDom.classList.toggle('ok', delta === 0);
         }
     }
 
-    putInReserve(token) {
-        const value = parseInt(token.dataset.value);
-        let tokenBefore = this.reserve.firstElementChild;
+    refreshDom() {
+        this.updateCalculations();
 
-        while (tokenBefore && parseInt(tokenBefore.dataset.value) < value) {
-            tokenBefore = tokenBefore.nextElementSibling;
+        for (const [token, slot] of this.placement.entries()) {
+            const tokenDom = this.shadowRoot.querySelector(`.token[data-value="${token + 1}"`);
+            let container = this.shadowRoot.querySelector(`.slot[data-position="${slot}"`);
+            if (!container) container = this.reserve;
+
+            container.appendChild(tokenDom);
         }
-
-        this.reserve.insertBefore(token, tokenBefore);        
     }
 }
 
